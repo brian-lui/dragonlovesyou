194c3c9e17ca777d8b041a380d5ad0703073167a
Brian <inaspanishway@gmail.com>
2020-10-17 13:59:42 +1100

    Window resizing with 1/2/3

--- game.lua
+++ game.lua
@@ -434,9 +434,21 @@ end
 function Game:keypressed(key)
 	if key == "escape" then
 		love.event.quit()
-
-	-- elseif, or whatever
-
+	elseif key == "1" then
+		local aspectRatio = 1920/1080 -- desktop
+		local width = love.window.getMode()
+		local newheight = width / aspectRatio
+		love.window.setMode (width, newheight, {resizable = true})
+	elseif key == "2" then
+		local aspectRatio = 2732/2048 -- iPad
+		local width = love.window.getMode()
+		local newheight = width / aspectRatio
+		love.window.setMode (width, newheight, {resizable = true})
+	elseif key == "3" then
+		local aspectRatio = 2436/1125 -- mobile
+		local width = love.window.getMode()
+		local newheight = width / aspectRatio
+		love.window.setMode (width, newheight, {resizable = true})
 	end
 end
 
